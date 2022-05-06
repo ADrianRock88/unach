f8668b152296102cdd885b8307d2b436a8b23cea
ADrianRock88 <104638543+ADrianRock88@users.noreply.github.com>
2022-05-06 10:05:09 -0500

    Update python_final.py

--- python_final.py
+++ python_final.py
@@ -36,3 +36,27 @@ for index in range (0,2,1):
 for i in arralum:
     for ii in i:
         print(ii)
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  6 09:03:33 2022
+
+@author: ADrian Solis
+"""
+
+from numpy import array 
+import numpy as np
+
+dtype=[('nombre',(np.str_,40)),('matricula',(np.str_,8)),('semestre',np.int32),('promedio',np.float64)]
+
+scnom=input("Inserte el nombre ")
+scnom=str(scnom)
+scmat=input("Inserte la matricula ")
+scmat=str(scmat)
+scsem=input("Inserte el semestre")
+scsem=int(scsem)
+scpro=input("Inserte el promedio")
+scpro= float(scpro)
+
+
+
+stru=np.array([scnom,scmat,scsem,scpro],dtype=dtype)
