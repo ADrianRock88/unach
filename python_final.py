1f55cb888a8543439cdca99191a48fee5ffc4cb0
ADrianRock88 <104638543+ADrianRock88@users.noreply.github.com>
2022-05-07 22:55:37 -0500

    Update python_final.py

--- python_final.py
+++ python_final.py
@@ -1,62 +1,16 @@
-alumno=[]
-arralum=[]
-
-"""sknnom=input("Insertar el nombre del alumno: ")
-sknmatri=input("Inserte la matricula del alumno: ")
-sknsem=input("Inserte el semestre del alumno: ")
-sknsem = int (sknsem)
-sknprom=input("Inserte el promedio del alumno: ")
-sknprom= float(sknprom)
-
-alumno.append(sknnom)
-alumno.append(sknmatri)
-alumno.append(sknsem)
-alumno.append(sknprom)"""
-
-for index in range (0,2,1):
-    
-    sknnom=input("Insertar el nombre del alumno: ")
-    sknmatri=input("Inserte la matricula del alumno: ")
-    sknsem=input("Inserte el semestre del alumno: ")
-    sknsem = int (sknsem)
-    sknprom=input("Inserte el promedio del alumno: ")
-    sknprom= float(sknprom)
-    alumno.append(sknnom)
-    alumno.append(sknmatri)
-    alumno.append(sknsem)
-    alumno.append(sknprom)
-    print("---", index)
-    print("llegada")
-    arralum.append(alumno)    
-    print("¡¡¡¡¡¡¡¡",arralum,"!!!!!!!")
-    
-    
-    
-    
-for i in arralum:
-    for ii in i:
-        print(ii)
 # -*- coding: utf-8 -*-
 """
 Created on Fri May  6 09:03:33 2022
-
 @author: ADrian Solis
 """
 
-from numpy import array 
+
 import numpy as np
-
 dtype=[('nombre',(np.str_,40)),('matricula',(np.str_,8)),('semestre',np.int32),('promedio',np.float64)]
+strarray=np.array([("ADrian","B221038",1,9.5),("Felipe","B222344",2,8.0),("Fernanda","B201293",4,10.0),("Julieta","B213465",6,8.9),("Ernesto","B208975",5,9.9),("Marta","B191342",3,6.0),("John","B145678",8,7.0),("Linda","B187654",2,6.5),("Francisco","B223412",4,7.8),("Maria","B190047",8,6.7)],dtype=dtype)
+arreglado=np.sort(strarray, order=('promedio'))
+print("Los regitros son\n",arreglado)
+print("Los becado son:")
+for i in range(7,10):
+    print(arreglado[i])
 
-scnom=input("Inserte el nombre ")
-scnom=str(scnom)
-scmat=input("Inserte la matricula ")
-scmat=str(scmat)
-scsem=input("Inserte el semestre")
-scsem=int(scsem)
-scpro=input("Inserte el promedio")
-scpro= float(scpro)
-
-
-
-stru=np.array([scnom,scmat,scsem,scpro],dtype=dtype)
